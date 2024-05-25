d4c396fad6da86f711ce6ec9e3b801e32ab6fd39
Hiro Mia <hiromia006@gmail.com>
2024-05-25 21:10:05 +0600

    Selenium Batch Nine first class

--- src/test/java/class1/BrowserLaunch.java
+++ src/test/java/class1/BrowserLaunch.java
@@ -5,8 +5,12 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BrowserLaunch {
     public static void main(String[] args) throws InterruptedException {
-        WebDriver driver=new FirefoxDriver();
+        WebDriver driver = new FirefoxDriver();
         driver.get("https://www.google.com/");
+
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+
         Thread.sleep(2000);
         driver.quit();
     }
