e04c917b61f63bfef964e13c9a5797d2cae6bf3c
Hiro Mia <hiromia006@gmail.com>
2024-05-29 21:49:05 +0600

    Selenium Batch Nine 4th  class

--- src/test/java/class4/TabsTest.java
+++ src/test/java/class4/TabsTest.java
@@ -1,13 +1,41 @@
 package class4;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TabsTest {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new FirefoxDriver();
-        driver.get("https://demoqa.com/frames");
+        driver.get("https://demoqa.com/browser-windows");
         driver.manage().window().maximize();
+
+        String parentPage=driver.getWindowHandle();
+        driver.getWindowHandles();
+        System.out.println("Main Window : " + driver.findElement(By.className("text-center")).getText());
+
+        Thread.sleep(3000);
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.navigate().to("https://demoqa.com/sample");
+        System.out.println("Inside Child : " + driver.findElement(By.id("sampleHeading")).getText());
+        Thread.sleep(3000);
+        driver.close();
+
+        driver.switchTo().window(parentPage);
+        Thread.sleep(3000);
+
+        System.out.println("Second Time : " + driver.findElement(By.className("text-center")).getText());
+        driver.switchTo().newWindow(WindowType.WINDOW);
+        driver.navigate().to("https://demoqa.com/sample");
+        System.out.println("Inside Child : " + driver.findElement(By.id("sampleHeading")).getText());
+        Thread.sleep(3000);
+        driver.close();
+
+        driver.switchTo().window(parentPage);
+        Thread.sleep(3000);
+
+
 
         driver.quit();
     }
--- src/test/java/class4/iFramesTest.java
+++ src/test/java/class4/iFramesTest.java
@@ -11,16 +11,16 @@ public class iFramesTest {
         driver.manage().window().maximize();
 
         System.out.println("Main iFrame : " + driver.findElement(By.className("text-center")).getText());
-        Thread.sleep(200);
+        Thread.sleep(2000);
         driver.switchTo().frame("frame1");
         System.out.println("Inside iFrame1 : " + driver.findElement(By.id("sampleHeading")).getText());
         driver.switchTo().defaultContent();
 
-        Thread.sleep(200);
+        Thread.sleep(2000);
         driver.switchTo().frame("frame2");
         System.out.println("Inside iFrame2 : " + driver.findElement(By.id("sampleHeading")).getText());
         driver.switchTo().defaultContent();
-        Thread.sleep(200);
+        Thread.sleep(2000);
 
         driver.quit();
     }
