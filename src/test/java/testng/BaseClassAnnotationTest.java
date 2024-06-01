149b82feb24b4e43870f7b67a62cf029a992a471
Hiro Mia <hiromia006@gmail.com>
2024-06-01 22:50:02 +0600

    TestNG 1st class

--- src/test/java/testng/BaseClassAnnotationTest.java
+++ src/test/java/testng/BaseClassAnnotationTest.java
@@ -3,9 +3,7 @@ package testng;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 
 import java.time.Duration;
 
